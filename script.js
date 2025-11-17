750bcc41465d9b7a865aa35bb6d3ac791b28659b
Felipe Ponce de León Espinosa <felipeponcedeleon@outlook.com>
2025-11-17 20:53:10 -0300

    Update

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const newQuoteBtn = document.getElementById('new-quote')
+
+
 let apiQuotes = [];
 
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     console.log(quote);
+
+    // Verificar si la cita tiene autor
+    if (!quote.author) {
+        authorText.textContent = 'Anónimo';
+    } else {
+        authorText.textContent = quote.author;
+    }
+   
+    // Verificar longitud para manejar estilo
+    if (quote.text.length > 50) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+    quoteText.textContent = quote.text;
+
 }
 
 async function getQuotes() {
@@ -21,5 +44,7 @@ async function getQuotes() {
         console.error('Error al cargar quotes.json:', error);
     }
 }
+
+newQuoteBtn.addEventListener('click', newQuote);
 
 getQuotes();
